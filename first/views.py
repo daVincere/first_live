75b26b7afd217ce537eb0db1a3e57eeceb40f983
Hardeep <hardeepsinghgaff@gmail.com>
2017-02-09 21:27:02 +0530

    some additions done

--- first/views.py
+++ first/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
 	return render(request, 'about.html', {})
+
+def default(request):
+	return render(request, 'default.html', {})
--- live/urls.py
+++ live/urls.py
@@ -20,5 +20,6 @@ from first import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^about/', views.about, name='about'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^progress$', views.default, name='default'),
 ]
