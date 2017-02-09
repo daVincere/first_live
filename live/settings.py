008caec7eb9b4b110a1a964616358e13de4ce3d5
Hardeep <hardeepsinghgaff@gmail.com>
2017-02-10 01:47:47 +0530

    new static root added

--- live/settings.py
+++ live/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_here', 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static_here', 'media')
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_URL = '/media/'
 
 
 STATICFILES_DIRS = [
